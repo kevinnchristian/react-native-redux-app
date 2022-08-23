f09ba81ca5b2bfe399951f3596b5b27b18a03809
kevinnchristian <kevin.cmribeiro@outlook.com>
2022-08-23 19:35:17 -0300

    :tada: initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const AppStartup = () => {
+  return (
+    <View>
+      <Text>Hello Redux!</Text>
+    </View>
+  );
+}
+
+export default AppStartup;
